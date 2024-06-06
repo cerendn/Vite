7202c09b0d748698c51dc666bebba419d6a6cdef
cerendn <cerendncer@outlook.com>
2024-06-06 20:15:35 +0300

    createRef

--- student-manager/src/pages/Reducer.jsx
+++ student-manager/src/pages/Reducer.jsx
@@ -1,16 +1,16 @@
-import React, { useReducer } from "react";
+import React, { useReducer, createRef } from "react";
 
 const initialState = { counter: 0, lastUpdated: new Date().toLocaleString() };
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case "increment by":
+    case "increment_by":
       return {
         ...state,
         counter: state.counter + 1,
         lastUpdated: new Date().toLocaleDateString(),
       };
-    case "decrement by":
+    case "decrement_by":
       return {
         ...state,
         counter: state.counter - 1,
@@ -40,6 +40,8 @@ const reducer = (state, action) => {
         counter: action.payload.amount,
         lastUpdated: new Date().toLocaleString(),
       };
+    case "ERROR":
+      return { ...state, error: action.payload.error };
 
     default:
       return state;
@@ -48,13 +50,62 @@ const reducer = (state, action) => {
 
 const Reducer = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const incByRef = createRef();
+  const decByRef = createRef();
+  const setByRef = createRef();
 
   return (
     <div>
       <p>Counter:{state.counter} </p>
-      <button onClick={() => dispatch({ type: "increment" })}>İncrement</button>
-      <button onClick={() => dispatch({ type: "decrement" })}>Decrement</button>
+      <p>Last Updated:{state.lastUpdated} </p>
+
+      <button onClick={() => dispatch({ type: "increment_by" })}>
+        İncrement
+      </button>
+      <button onClick={() => dispatch({ type: "decrement_by" })}>
+        Decrement
+      </button>
       <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
+      <br />
+      <button
+        onClick={() =>
+          dispatch({
+            type: "INCREMENT",
+            payload: {
+              amount: Number(incByRef.current.value),
+            },
+          })
+        }
+      >
+        inc
+      </button>
+      <input type="text" placeholder="Increment Amount" ref={incByRef} />
+      <br />
+      <button
+        onClick={() =>
+          dispatch({
+            type: "DECREMENT",
+            payload: {
+              amount: Number(decByRef.current.value),
+            },
+          })
+        }
+      >
+        dec
+      </button>
+      <input type="text" placeholder="Decrement Amount" ref={decByRef} />
+
+      <button
+        onClick={() =>
+          dispatch({
+            type: "SET_COUNTER",
+            payload: { amount: Number(setByRef.current.value) },
+          })
+        }
+      >
+        SetCounter
+      </button>
+      <input type="text" placeholder="Set Counter" ref={setByRef} />
     </div>
   );
 };
